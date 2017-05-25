364de57aa8eea5cbb2c51033857d984757f7ce06
steveszc <steveszc@gmail.com>
2017-05-25 14:47:01 -0600

    fix navmenu syntax

--- js/navmenu.jq.js
+++ js/navmenu.jq.js
@@ -2,23 +2,21 @@
 ;(function(fn) {
     if (document.readyState != 'loading') { fn(); }
     else { document.addEventListener('DOMContentLoaded', fn); }
-})(
-    function() {
-        var smallMenu, bigMenu, breakM = 'screen and (min-width:48em)';
+})(function() {
+    var smallMenu, bigMenu, breakM = 'screen and (min-width:48em)';
 
-        // Enquire decides which menu script to load, based on breakM media query
-        enquire.register(breakM, {
-            setup: function() {
-                smallMenu = new rtd.DrawerMenu();
-            },
-            match: function() {
-                smallMenu._destroy();
-                bigMenu = new rtd.DropMenu();
-            },
-            unmatch: function() {
-                bigMenu._destroy();
-                smallMenu = new rtd.DrawerMenu();
-            }
-        }, true); // true flag for noquery fallbacks to match state
-    });
-);
+    // Enquire decides which menu script to load, based on breakM media query
+    enquire.register(breakM, {
+        setup: function() {
+            smallMenu = new rtd.DrawerMenu();
+        },
+        match: function() {
+            smallMenu._destroy();
+            bigMenu = new rtd.DropMenu();
+        },
+        unmatch: function() {
+            bigMenu._destroy();
+            smallMenu = new rtd.DrawerMenu();
+        }
+    }, true); // true flag for noquery fallbacks to match state
+});
