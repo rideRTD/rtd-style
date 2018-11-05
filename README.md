4b7245706fccb1c9716a849e99bfe4cd09118b75
Jay Nielsen <jayarnielsen@gmail.com>
2018-11-05 11:28:31 -0700

    added a local dev server

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Shared repo for RTD assets on [website-drupal8](https://github.com/rideRTD/websi
 
 ## Development
 
+- Run `npm start` to spin up a local, live-reloading development server at http://localhost:8080/.
 - Always lint (`npm run lint:css`) and build (`npm run build`) styles before committing.
 - Tag your commit with an incremental version change (e.g. v0.2).
 - Update the dependency in each project's package.json (e.g. git+https://github.com/rideRTD/rtd-style.git#v0.2)
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   entry: {
+    app: path.resolve(__dirname, 'js/app.js'),
     main: path.resolve(__dirname, 'scss/style.scss'),
     critical: path.resolve(__dirname, 'scss/critical.scss')
   },
@@ -16,6 +18,10 @@ const config = {
     new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'RTD Shared Styles Demonstration Template',
+      template: path.resolve(__dirname, 'html/index.html')
     })
   ],
   module: {
