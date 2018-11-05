0ca52d12a5ea592ea1a80871e4604f9ee49f3641
Jay Nielsen <jayarnielsen@gmail.com>
2018-11-05 11:31:03 -0700

    prettier formatted js files

--- js/navmenu.jq.js
+++ js/navmenu.jq.js
@@ -1,22 +1,31 @@
 /* global enquire, rtd */
-;(function(fn) {
-    if (document.readyState != 'loading') { fn(); }
-    else { document.addEventListener('DOMContentLoaded', fn); }
+(function(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
 })(function() {
-    var smallMenu, bigMenu, breakM = 'screen and (min-width:48em)';
+  var smallMenu,
+    bigMenu,
+    breakM = 'screen and (min-width:48em)';
 
-    // Enquire decides which menu script to load, based on breakM media query
-    enquire.register(breakM, {
-        setup: function() {
-            smallMenu = new rtd.DrawerMenu();
-        },
-        match: function() {
-            smallMenu._destroy();
-            bigMenu = new rtd.DropMenu();
-        },
-        unmatch: function() {
-            bigMenu._destroy();
-            smallMenu = new rtd.DrawerMenu();
-        }
-    }, true); // true flag for noquery fallbacks to match state
+  // Enquire decides which menu script to load, based on breakM media query
+  enquire.register(
+    breakM,
+    {
+      setup: function() {
+        smallMenu = new rtd.DrawerMenu();
+      },
+      match: function() {
+        smallMenu._destroy();
+        bigMenu = new rtd.DropMenu();
+      },
+      unmatch: function() {
+        bigMenu._destroy();
+        smallMenu = new rtd.DrawerMenu();
+      }
+    },
+    true
+  ); // true flag for noquery fallbacks to match state
 });
